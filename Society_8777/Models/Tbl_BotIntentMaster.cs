130b7887d25beb2bd8222943d79bb59a27eb0261
Pathrikar Atul <Pathrikaratul7@gmail.com>
2026-02-28 14:13:37 +0530

    Only Tbl_BotIntentMaster added

---
+++ Society_8777/Models/Tbl_BotIntentMaster.cs
@@ -0,0 +1,10 @@
+﻿namespace Society_8777.Models
+{
+    public class Tbl_BotIntentMaster
+    {
+        public long IntentId { get; set; }  
+        public string IntentCode { get; set; } = null!;
+        public int   IntentPriority { get; set; }  
+        public bool IsActive { get; set; }
+    }
+}
